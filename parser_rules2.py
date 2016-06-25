cf16e44e3b341ae73f2bb8b447b376c861fd8ac4
nicolas <klascolin@gmail.com>
2016-06-25 18:00:47 -0300

    AGrego control, sentencias y funciones

--- parser_rules2.py
+++ parser_rules2.py
@@ -10,7 +10,132 @@ class Node:
               self.children = [ ]
          self.leaf = leaf
 
+
+
 # Simbolo inicial
+start = 'g'
+
+# ---------------------------------------------------------------------------------------
+#Sentencias:
+
+#G -> SG | CtrlG
+def p_g1(subexpressions):
+	'''g : sentencia g'''
+
+def p_g2(subexpressions):
+	'''g : ctrl g'''
+
+def p_g3(subexpressions):
+	'''g : empty'''
+
+
+#S ->  VarOps ; | Func ;| return ;| varAsig;
+def p_sentencia1(subexpressions):
+	'''sentencia : varsOps ';' '''
+def p_sentencia2(subexpressions):
+	'''sentencia : func ';' '''
+
+# Faltaban las asignaciones, el print y el return
+def p_sentencia3(subexpressions):
+	'''sentencia : varAsig ';' '''
+
+#Esta ya esta capturada en func
+#def p_sentencia4(subexpressions):
+#	'''sentencia : funcVoid ';' '''
+
+def p_sentencia5(subexpressions):
+	'''sentencia : RETURN ';' '''
+
+#Ctrl -> IF| Loop
+def p_ctrl1(subexpressions):
+	'''ctrl : if'''
+def p_ctrl2(subexpressions):
+	'''ctrl : loop'''
+
+#Loop -> while(ExpBool) Bloque | do Bloque while(ExpBool); | for(VarAsig; ExpBool; )Bloque
+#en el tercer parametro del for pongo varOps, pero en realidad puede ser mas general(!), es lo que discutimos en clase.
+def p_loop1(subexpressions):
+	'''loop : WHILE '(' varExpresion')' bloque'''
+def p_loop2(subexpressions):
+	'''loop : DO bloque WHILE '(' varExpresion ')' ';' '''
+def p_loop3(subexpressions):
+	'''loop : FOR '(' varExpresion ';' varExpresion ';' varOps')' bloque '''
+
+# ---------------------------------------------------------------------------------------
+#Control:
+
+# Modifico para que el else sea opcional
+# IF-> if(ExpBool) then Bloque Else
+# Else -> else Bloque | lambda
+def p_if(subexpressions):
+	'''if : IF '(' varExpresion ')' THEN bloque else'''
+
+def p_else1(subexpressions):
+	'''else : ELSE bloque '''
+
+def p_else2(subexpressions):
+	'''else : empty'''
+
+#Bloque -> S | {G}
+def p_bloque1(subexpressions):
+	'''bloque : sentencia '''
+def p_bloque2(subexpressions):
+	'''bloque : '{' g '}' '''
+
+#-----------------------------------------------------------------------------
+#Funciones
+
+# Func -> FuncReturn | FuncVoid
+def p_func1(subexpressions):
+	'''func : funcReturn'''
+
+def p_func2(subexpressions):
+	'''func : funcVoid'''
+
+# FuncReturn -> FuncInt | FuncString | FuncBool
+def p_funcReturn1(subexpressions):
+	'''funcReturn : funcInt'''
+
+def p_funcReturn2(subexpressions):
+	'''funcReturn : funcString'''
+
+def p_funcReturn3(subexpressions):
+	'''funcReturn : funcBool'''
+
+# FuncInt -> multiplicacionEscalar(ID, EMat, Param) | length(Vec)
+
+#vec en realidad es la definicion de un vector o sea, a = [1,1]. Esto puede tener sentido o no, pero tambien podriamos pasarle una variable_
+#que ya es un vector como multiescalar(vector,2,) 
+def p_funcInt1(subexpressions):
+	'''funcInt : MULTIESCALAR '(' varExpresion ',' varExpresion ',' param ')' '''
+
+def p_funcInt2(subexpressions):
+	'''funcInt : LENGTH '(' varExpresion ')' '''
+
+# FuncString -> capitalizar(ExpString)
+def p_funcString(subexpressions):
+	'''funcString : CAPITALIZAR '(' varExpresion ')' '''
+
+# FuncBool -> colineales(Vec,Vec )
+def p_funcBool(subexpressions):
+	'''funcBool : COLINEALES '(' varExpresion ',' varExpresion ')' '''
+
+# FuncVoid -> print(Valores) 
+def p_funcVoid(subexpressions):
+	'''funcVoid : PRINT '(' varExpresion ')' '''
+
+#M -> ExpBool | lambda
+#Este M no tiene nada que ver con el M de arriba, que se usa en el parametro de una funcion. Lo cambio
+def p_param1(subexpressions):
+	'''param : varExpresion'''
+
+def p_param2(subexpressions):
+	'''param : empty'''
+
+def p_empty(subexpressions):
+	'''empty : '''
+
+
 
 precedence = (
 	('right','?'),
@@ -28,11 +153,6 @@ precedence = (
 	('left','NOT'),	
 )
 
-def p_varYVals(subexpressions):
-	'''varYVals : ID'''
-
-def p_varOps(subexpressions):
-	'''varOps : ID MASMAS'''
 
 def p_funcInt(subexpressions):
 	'''funcInt : LENGTH'''
@@ -109,6 +229,7 @@ def p_varAsig9(subexpressions):
 	'''varAsig : ID DIVEQ valores '''	
 def p_varAsig10(subexpressions):
 	'''varAsig : ID '=' valores '''
+
 
 
 
