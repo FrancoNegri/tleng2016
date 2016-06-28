27ed4d94ec138143af0060fd9d369bf9b5fb1f49
FranckN <franconegri2004@hotmail.com>
2016-06-28 11:06:14 -0300

    Agregado tester

---
+++ tester.sh
@@ -0,0 +1,15 @@
+rm testing/*.out 2> /dev/null
+rm errores.txt 2> /dev/null
+echo "Escribiendo errores en errores.txt"
+for f in testing/*.i
+do
+	python parser.py $f $f.out 2>> errores.txt
+	if [ $? -eq 0 ]
+	then
+		echo "Pasa: " $f
+	else
+  		echo "No Pasa: " $f
+	fi
+	#Esto es para ver cuando escribamos la salida:
+	#diff $f $f.out
+done
